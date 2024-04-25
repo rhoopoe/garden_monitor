2272853ce9b1f713800c996bfbc89ebe7a4b6861
rkukutis <kukutis.ricardas@gmail.com>
2024-04-25 14:10:01 +0300

    tried out stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {clicks:{type: Number, default: 0}}
+  static targets = ["bruh"]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.bruhTarget.innerHTML = this.clicksValue
+  }
+
+  add() {
+    this.clicksValue++;
+  }
+
+  clicksValueChanged() {
+    console.log("A button has been clicked");
+    this.bruhTarget.innerHTML = this.clicksValue;
   }
 }
